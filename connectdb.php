aeefc01bef6f482af0a4b2c458444b41db694b3a
IliusSagar <sagar01830@gmail.com>
2021-11-06 05:47:14 -0700

    try catch update connectiondb & Login code part-01

--- connectdb.php
+++ connectdb.php
@@ -1,6 +1,14 @@
-<?php 
+<?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=pos_db','root','');
-echo 'Connection Successfull';
+try{
+
+    $pdo = new PDO('mysql:host=localhost;dbname=pos_db','root','');
+    //echo 'Connection Successfull';
+
+}catch(PDOException $f){
+
+    echo $f->getmessage();
+
+}
 
 ?>

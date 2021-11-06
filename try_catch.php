0fc9bedbe5c6890ba7fff38badddb6dcf3cb46a4
IliusSagar <sagar01830@gmail.com>
2021-11-06 05:18:51 -0700

    try catch in php with database connection for user friendly error

---
+++ try_catch.php
@@ -0,0 +1,14 @@
+<?php
+
+try{
+
+    $pdo = new PDO('mysql:host=localhost;dbname=podb','root','');
+    echo 'Connection Successfull';
+
+}catch(PDOException $f){
+
+    echo $f->getmessage();
+
+}
+
+?>
